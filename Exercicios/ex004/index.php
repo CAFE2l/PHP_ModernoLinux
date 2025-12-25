cbb6d397bd60f9fb08384994cf1e2be04cba7d91
CAFE2l <pciskolargx@gmail.com>
2025-12-25 14:16:30 -0300

    exercicio 04 criado

---
+++ Exercicios/ex004/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+        $num = 300;
+        echo "O valor da variável é $num";
+    
+    ?>
+</body>
+</html>
