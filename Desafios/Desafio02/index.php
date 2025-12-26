dc0d638885154f3e2a35b4c89edc23c313dcbf2a
CAFE2l <pciskolargx@gmail.com>
2025-12-26 11:04:00 -0300

    desafio 2 finalizado

---
+++ Desafios/Desafio02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorteador de números</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Trabalhando com números aleatórios</h1>
+    </header>
+    <main>
+        <p>Gerando um número aleatório entre 0 e 100...</p>
+        <p>O valor gerado foi <?php echo rand(0, 100); ?></p>
+        <button onclick="location.reload()">Gerar outro número</button>
+    </main>
+</body>
+</html>
