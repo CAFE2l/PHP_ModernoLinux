cbdd21fd9b67bce9009dfdbe628901029ca52689
CAFE2l <pciskolargx@gmail.com>
2025-12-26 12:27:42 -0300

    solução do guana finalizada

---
+++ Soluções/Solucao1/resposta.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado Final</h1>
+    </header>
+    <main>
+        <p>
+            <?php 
+                $n = $_REQUEST["num"];
+                $ant = $n - 1;
+                $suc = $n + 1;  
+
+                echo "O antecessor de $n é $ant <br>";
+                echo "O sucessor de $n é $suc";
+            ?>
+        </p>
+    </main>
+</body>
+</html>
