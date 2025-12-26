b4ab48988847855ac8691c56488f2529845dc812
CAFE2l <pciskolargx@gmail.com>
2025-12-26 10:46:51 -0300

    funções importadas finalizadas

---
+++ Exercicios/ex007/index2.php
@@ -0,0 +1,14 @@
+<?php 
+    $r = base_convert(1011, 2, 10);
+    echo "O valor é $r";
+    $res = intdiv(5,2);
+    echo "A resposta é $res";
+    //calculando o valor de pi
+    $result = pi();
+    $constante = M_PI;  
+    echo "O valor de pi é $result e a constante é $constante";
+
+    //calculando potencia na  versão 5
+    $potencia = pow(2, 3);
+    echo "A potência é $potencia";
+?>
