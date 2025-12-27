1e3c77e80050a01e80d0c0aef702b548b1801138
CAFE2l <pciskolargx@gmail.com>
2025-12-27 12:24:44 -0300

    testando

---
+++ Exercicios/ex009/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    //Capcturando os dados do formulario retroalimentado
+
+        $valor1 = $_GET['v1'] ?? 0;
+        $valor2 = $_GET['v2'] ?? 0;
+
+        $soma = $valor1 + $valor2;
+
+
+    ?>
+
+    <main>
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
+            <label for="v1">Valor 1</label>
+            <input type="number" name="v1" id="v1" value="<?= $valor1 ?>" required>
+
+            <label for="v2">Valor 2</label>
+            <input type="number" name="v2" id="v2" value="<?= $valor2 ?>" required>
+            <input type="submit" value="Calcular">
+        </form>
+    </main> 
+</body>
+</html>
