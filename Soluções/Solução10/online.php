27a6849f22edb845d32d22da0425487d9bf6d232
CAFE2l <pciskolargx@gmail.com>
2025-12-28 11:58:12 -0300

    solução10 finalizada

---
+++ Soluções/Solução10/online.php
@@ -0,0 +1,10 @@
+<?php 
+    $atual = date("Y");
+    echo " $atual - Todos os direitos reservados.";
+    $nasc = 1978;
+    $ano = $autal;
+
+    $idade = $ano - $nasc;
+
+    echo "Quem nasceu em $nasc, vai ter $idade anos em $ano";
+?>
